4aa88f6a624d45cfeb5b2173f58700223fef35a8
Ariel Wang <arielwang727@gmail.com>
2026-06-17 22:47:51 -0700

    Update birthday_paradox.py

--- birthday_paradox.py
+++ birthday_paradox.py
@@ -1,11 +1,6 @@
 import random
 
-people = 23 
-
-birthdays = []
-
-for i in range(people):
-  birthday = random.randint(1,365)
-  birthdays.append(birthday)
+people = 23
+birthdays = [random.randint(1, 365) for _ in range(people)]
 
 print(birthdays)
